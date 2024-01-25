4007cf1014143611b1911c1d1962ab19a04dd204
Swapnil Opash <sra.opash@gmail.com>
2024-01-25 19:06:40 +0530

    fixed

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -106,7 +106,9 @@ exports.notificationMail = async (userData) => {
 
 exports.channelNotificationEmail = async (userData) => {
   let name = userData?.Username;
-  let msg = `you are assigned in HT channel by MasterAdmin`;
+  let msg = `You have been assign in OrganicDating channel by the OrganicDating Admin.
+  To access your channel, log into your OrganicDating account,click on the
+  OrganicDating icon at the top of the page,then click on My Channel.`;
   let redirectUrl = `${environment.FRONTEND_URL}`;
 
   const mailObj = {
@@ -131,9 +133,9 @@ exports.communityApproveEmail = async (profileId, isApprove) => {
       userData[0]?.FirstName + " " + userData[0]?.LastName;
     let msg = "";
     if (isApprove === "Y") {
-      msg = `Your Connection has been approved by Master Admin.`;
+      msg = `OrganicDating has approved your Connection application.`;
     } else {
-      msg = `Your Connection has been unapproved by Master Admin.`;
+      msg = `OrganicDating has unapproved your Connection application.`;
     }
     let redirectUrl = `${environment.FRONTEND_URL}`;
     const mailObj = {
